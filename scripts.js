f5084203a75ada78666c37a543f09c05b236a57b
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-02 13:58:25 -0700

    add question/answer divs in html body
    co-authored-by: Matt-McFarland <mrmcfarland@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#cardOne").find(button).click(function() {
+    $("#cardOneDef").toggleClass("hidden");
+  });
+});
+
+// $("button#light-mode").click(function() {
+//   $("body").removeClass();
+//   $("body").addClass("light-mode");
+// });
